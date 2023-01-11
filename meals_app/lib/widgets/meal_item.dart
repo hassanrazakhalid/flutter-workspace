930e81cac358a39057958e1a673c4570e7521773
hassan janjua <hassanrazakhalid@yahoo.com>
2023-01-11 18:48:41 +0500

    navigation to meal

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -18,6 +18,38 @@ class MealItem extends StatelessWidget {
       required this.complexity,
       required this.affordability});
 
+  String get complexityText {
+    var val = '';
+    switch (complexity) {
+      case Complexity.Simple:
+        val = 'Simple';
+        break;
+      case Complexity.Challenging:
+        val = 'Challenging';
+        break;
+      case Complexity.Hard:
+        val = 'Hard';
+        break;
+    }
+    return val;
+  }
+
+  String get affordaabilityText {
+    var res = '';
+    switch (affordability) {
+      case Affordability.Affordable:
+        res = 'Affordable';
+        break;
+      case Affordability.Pricey:
+        res = 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        res = 'Luxurious';
+        break;
+    }
+    return res;
+  }
+
   void selectMeal() {}
 
   @override
@@ -30,24 +62,82 @@ class MealItem extends StatelessWidget {
         ),
         elevation: 4,
         margin: EdgeInsets.all(10),
-        child: Column(children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  topRight: Radius.circular(15),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Image.network(
-                  imageUrl,
-                  height: 250,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+                Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 ),
-              )
-            ],
-          )
-        ]),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${duration} min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${complexityText}')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${affordaabilityText}')
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
